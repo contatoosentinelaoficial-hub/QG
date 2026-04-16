efdbb4230d9ce635a03af66a508d84db8c3b69d0
contatoosentinelaoficial-hub <contato.osentinela.oficial@gmail.com>
2026-04-16 16:12:43 -0300

    Update checkout links and comments in config.dart

--- lib/config.dart
+++ lib/config.dart
@@ -1,9 +1,12 @@
 class AppConfig {
   // A CHAVE DE BIFURCAÇÃO: 
   // Mude para 'true' quando for gerar o APK Vitalício. 
-  // Deixe 'false' para gerar o APK Anual (que cobra o Upgrade).
+  // Deixe 'false' para gerar o APK Anual ou o PWA Isca Web.
   static const bool isVitalicio = false;
 
-  // O SEU LINK DE EXTRAÇÃO SECRETO
-  static const String linkCheckout = 'https://pay.kiwify.com.br/3YkOUBk';
+  // 🎯 LINK 1: ALVO NOVATO (Amostra Grátis) -> Oferta do Combo
+  static const String linkCheckoutCombo = 'https://pay.kiwify.com.br/evuxf9d';
+
+  // 🎯 LINK 2: ALVO VETERANO (Plano Anual) -> Oferta de Upgrade Direto
+  static const String linkCheckoutVitalicio = 'https://pay.kiwify.com.br/3YkOUBk';
 }
