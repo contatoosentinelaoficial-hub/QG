b97d95861e5cbb18ce5cbfc3b36251d8760003a8
contatoosentinelaoficial-hub <contato.osentinela.oficial@gmail.com>
2026-04-16 02:54:28 -0300

    Add AppConfig class with configuration constants

---
+++ lib/config.dart
@@ -0,0 +1,9 @@
+class AppConfig {
+  // A CHAVE DE BIFURCAÇÃO: 
+  // Mude para 'true' quando for gerar o APK Vitalício. 
+  // Deixe 'false' para gerar o APK Anual (que cobra o Upgrade).
+  static const bool isVitalicio = false;
+
+  // O SEU LINK DE EXTRAÇÃO SECRETO
+  static const String linkCheckout = 'https://pay.kiwify.com.br/3YkOUBk';
+}
